611e0f0cfece63a77cd3ef16e2235a8559f8fcc2
Pat Thomas <pthomas@relaynetwork.com>
2015-04-22 10:12:00 -0400

    Port over timed-loop.

--- src/cereal/core.clj
+++ src/cereal/core.clj
@@ -1,6 +1,25 @@
-(ns cereal.core)
+(ns cereal.core
+  (:require [clojure.core.async :as a]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def stop-val (atom false)) ;; maybe this should be made into a map to support starting/stopping multiple concurrent loops
+
+(defn stop
+  []
+  (reset! stop-val true))
+
+(defn start
+  []
+  (reset! stop-val false))
+
+(defn timed-loop
+  [wait input-ch handler]
+  (let [ch (a/chan)]
+    (a/go
+      (while (not @stop-val)
+        (a/<! (a/timeout wait))
+        (handler (a/<! ch))))
+    (a/go
+      (while (not @stop-val)
+        (a/<! (a/timeout wait))
+        (a/>! ch (a/<! input-ch))))
+    input-ch))
